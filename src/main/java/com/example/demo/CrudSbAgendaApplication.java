78799ef1f11f0c4695d9282a8d9db0b718185b71
Vladimir <vla1305@hotmail.com>
2022-05-30 14:42:47 -0500

    Iniciado

---
+++ src/main/java/com/example/demo/CrudSbAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSbAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSbAgendaApplication.class, args);
+	}
+
+}
